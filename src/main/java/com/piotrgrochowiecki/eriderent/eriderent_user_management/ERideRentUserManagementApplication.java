7c027b0aa215b5a46ce18884a7dd367b59100d61
Piotr Grochowiecki <Piotr.Grochowiecki@dxc.com>
2023-10-16 21:24:19 +0200

    Initial commit

---
+++ src/main/java/com/piotrgrochowiecki/eriderent/eriderent_user_management/ERideRentUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.eriderent.eriderent_user_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERideRentUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERideRentUserManagementApplication.class, args);
+    }
+
+}
